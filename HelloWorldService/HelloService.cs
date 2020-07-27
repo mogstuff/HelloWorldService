a5cd8d4d8973b1634e84a42d42b2d4bd4d035694
Mark Morgan <markmorgan.mcp@gmail.com>
2020-07-27 15:25:56 +0100

    initial commit

---
+++ HelloWorldService/HelloService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Timers;
+using Topshelf.Logging;
+
+namespace HelloWorldService
+{
+  public  class HelloService
+    {
+        readonly Timer _timer;
+
+        private static readonly LogWriter _log = HostLogger.Get<HelloService>();
+
+
+
+        public HelloService()
+        {
+            _timer = new Timer(5000) { AutoReset = true };
+            _timer.Elapsed += WriteHello;            
+        }
+
+        private void WriteHello(object sender, ElapsedEventArgs e)
+        {
+
+            _log.InfoFormat($"Executing WriteHello command at {DateTime.Now}");
+
+            // sample code shamelessly stolen from here:
+            // https://docs.microsoft.com/en-us/dotnet/standard/io/how-to-open-and-append-to-a-log-file
+
+            string filePath = @"c:\temp\hello.txt";          
+       
+            using (StreamWriter w = File.AppendText(filePath))
+            {
+                Log("Test1", w);
+                Log("Test2", w);
+            }
+
+        }
+
+        public static void Log(string logMessage, TextWriter w)
+        {
+            w.Write("\r\nLog Entry : ");
+            w.WriteLine($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()}");
+            w.WriteLine("  :");
+            w.WriteLine($"  :{logMessage}");
+            w.WriteLine("-------------------------------");
+        }
+               
+        public void Start() { _timer.Start(); }
+        public void Stop() { _timer.Stop(); }
+
+        public void Pause() 
+        {
+            _log.InfoFormat($"HelloWorldService Paused at {DateTime.Now}");
+        }
+
+        public void Continue() 
+        {
+            _log.InfoFormat($"HelloWorldService Continued at {DateTime.Now}");
+        }
+
+        public void CustomCommand(int commandNumber) 
+        {
+            _log.InfoFormat("Hey, I got the command number '{0}'", commandNumber);
+        }
+
+    }
+}
---
+++ HelloWorldService/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Timers;
+using Topshelf;
+
+namespace HelloWorldService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rc = HostFactory.Run(x =>                                  
+            {
+                x.Service<HelloService>(s =>                                 
+                {
+                    s.ConstructUsing(name => new HelloService());             
+                    s.WhenStarted(tc => tc.Start());                        
+                    s.WhenStopped(tc => tc.Stop());                         
+                });
+                x.RunAsLocalSystem();                                     
+
+                x.SetDescription("This service writes to a log file every few seconds");                 
+                x.SetDisplayName("Hello World Service");                
+                x.SetServiceName("HelloWorldService");
+
+                x.EnableServiceRecovery(recoveryOption =>
+                {
+                    recoveryOption.RestartService(5); // wait5 minutes then restart service
+                    recoveryOption.RestartComputer(60, "wait an hour then reboot");
+                    recoveryOption.RunProgram(30,
+                        @"c:\snafu.exe"); // run this program after 30 minutes
+                });
+
+                x.StartAutomatically();
+
+                x.EnablePauseAndContinue();
+
+            });
+
+       
+
+            var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());  
+            Environment.ExitCode = exitCode;
+
+        }
+
+
+    }
+}
